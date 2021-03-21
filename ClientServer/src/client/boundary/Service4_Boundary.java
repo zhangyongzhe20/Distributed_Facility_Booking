a1cbcbf000a62cf6dc7f209cf03ee420cb5b7521
zhangyongzhe20 <jcddyongzhe@gmail.com>
2021-03-21 14:36:53 +0800

    service4 of client: need testing when server side is done

--- ClientServer/src/client/boundary/Service4_Boundary.java
+++ ClientServer/src/client/boundary/Service4_Boundary.java
@@ -1,14 +1,59 @@
 package client.boundary;
 
+import client.control.Service4Control;
+import java.io.IOException;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+
 public class Service4_Boundary extends Boundary {
+    private Service4Control s4C;
 
-    @Override
-    public void displayMain() {
-
+    public Service4_Boundary() throws SocketException, UnknownHostException {
+        this.s4C = new Service4Control();
     }
 
     @Override
-    public void displayReply() {
+    public void displayMain() throws Exception {
+        enterFacilityName();
+        enterMonitorIntervals();
 
+        //marshal
+        s4C.marshal();
+        // catch the socket timeout exception to exist the monitoring loop
+        // this socket timeout >= interval
+        try {
+            //todo: need testing
+        while(true) {
+                s4C.monitoring();
+                if (s4C.isNewUpdate){
+                    //handle response and display reply
+                    displayReply();
+                    s4C.isNewUpdate = false;
+                }
+            }
+        }catch (IOException e) {
+            System.err.println(e.getMessage());
+            // restore the timeout to be the default
+            //todo: need testing
+            s4C.setInterval(s4C.maxTimeout);
+        }
+    }
+
+
+
+    @Override
+    public void displayReply() {
+        //todo
+        System.out.println("Available intervals: " + s4C.unMarshal());
+    }
+
+    private void enterFacilityName() {
+        String name = readInputString("Enter the Facility name that you want to monitor: ");
+        s4C.setFacName(name);
+    }
+    private void enterMonitorIntervals() {
+        int interval = readInputInteger("Enter your previous booking ID: ");
+        s4C.setInterval(interval);
     }
 }
---
+++ ClientServer/src/client/control/Service4Control.java
@@ -0,0 +1,69 @@
+package client.control;
+
+import utils.UnMarshal;
+
+import java.io.IOException;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+
+public class Service4Control extends Control implements marshal, unmarshal{
+    private static final int SERVICEID = 4;
+    public boolean isNewUpdate;
+    private String name;
+    private int interval;
+
+    public Service4Control() throws SocketException, UnknownHostException {
+        super();
+        this.collectedData = new ArrayList<>();
+        this.marShalData = new byte[0];
+        this.unMarShalData = new byte[0];
+    }
+
+    /**
+     *  Marshal service data
+     */
+    public void marshal() throws Exception {
+            // header represents this is a request/response msg
+            collectedData.add(DataMSG);
+            // message id
+            collectedData.add(this.getMsgID());
+            collectedData.add(SERVICEID);
+            collectedData.add(name);
+            collectedData.add(interval);
+        System.out.println("data collect of service4: " + collectedData);
+        marShalData = marshalMsg(collectedData, false);
+            sendAndReceive(marShalData);
+    }
+
+    public int unMarshal() {
+        int actual_data = -1;
+        if(this.unMarShalData.length != 0) {
+            int isAck = UnMarshal.unmarshalInteger(this.unMarShalData, 0);
+            if (isAck == 0) {
+                System.err.println("Unsupported operation!!!");
+                return -2;
+            }
+            // actual data
+            actual_data = UnMarshal.unmarshalInteger(this.unMarShalData, 4);
+            System.out.println(actual_data);
+        }
+        return actual_data;
+    }
+
+    public void setFacName(String name) { this.name = name;}
+
+
+    public void setInterval(int interval) {
+        this.interval = interval;
+        // to set udp timeout to be the interval
+        this.maxTimeout = interval;
+    }
+
+    public void monitoring() throws IOException {
+            // if receive update from server
+            if ((this.unMarShalData = udpClient.UDPreceive()) != null){
+                this.isNewUpdate = true;
+            }
+    }
+}
